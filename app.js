9f0a2fd54133b3e0632f960770c19cc0422bec5c
Omkar Patil <omkar.s.patil-1@ou.edu>
2018-01-23 18:36:35 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express =require('express');
+const stripe = require('stripe')('sk_test_foujUppKSTYKjnAi6NYH2nt6');
+const bodyParser = require('body-parser');
+const exphndbr = require('express-handlebars');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+ app.engine('handlebars', exphndbr({defaultLayout: 'main'}));
+ app.set('view engine', 'handlebars');
+
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({extended:false}));
+
+
+ app.use(express.static(`${__dirname}/public`));
+
+
+ app.get('/', (req, res) => {
+    res.render('index');
+ });
+
+ app.post('/charge', (req, res) => {
+     const amount = 2500;
+     stripe.customers.create({
+         email: req.body.stripeEmail,
+         source: req.body.stripeToken
+     }).then(customer => stripe.charges.create({
+         amount,
+         description: ' By Omkar Patil',
+         currency: 'usd',
+         customer: customer.id
+     })).then(charge => res.render('success'));
+ });
+
+app.listen(port,() => {
+   console.log(`Server Started on port ${port}`);
+});
